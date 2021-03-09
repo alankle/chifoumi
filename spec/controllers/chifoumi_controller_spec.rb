60f25463e7129154339c4ef82c846ff81a779edd
Alan Kleber <alankle@gmail.com>
2021-03-09 15:16:45 +0100

    resdme

--- spec/controllers/chifoumi_controller_spec.rb
+++ spec/controllers/chifoumi_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Api::V1::ChifoumiController, type: :controller do
   context 'Create a new move' do
     it 'Returns Success Code and Result object' do
       post :new, params: { name: 'alan', move: 'rock' }
-      #expect(response.code).to eq('200')
+      # expect(response.code).to eq('200')
       hash_body = nil
       expect { hash_body = JSON.parse(response.body).with_indifferent_access }.not_to raise_exception
       expect(hash_body.keys).to match_array(%w[moves result])
